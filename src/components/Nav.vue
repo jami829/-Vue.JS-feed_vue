4096688a1e86470cfeb9406ba8f9f4b4d19c41f9
이재훈 <jami8291215@gmail.com>
2021-02-07 16:44:08 +0900

    nav scss 조정

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="Nav">Nav 바입니다.</div>
+  <div id="nav">
+    <div id="contents">'[제출일] 이름'을 작성해주세요 :)</div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +9,17 @@ export default {
   name: "nav",
 };
 </script>
+
+<style lang="scss" scoped>
+#nav {
+  height: 70px;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.07);
+  display: flex;
+  align-items: center;
+
+  #contents {
+    font-size: 16px;
+    margin-left: 190px;
+  }
+}
+</style>
