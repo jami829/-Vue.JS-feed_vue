bd01ebf9c17e7df9ba972f995cc8683c98548b16
이재훈 <jami8291215@gmail.com>
2021-02-07 19:19:19 +0900

    axios 설치 및 list 정보 받아옴

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="main">
-    <div id="login_btn">로그인</div>
+    <div id="login_btn" @click="setData">로그인</div>
     <div>
       <Card />
     </div>
@@ -14,6 +14,27 @@ export default {
   name: "Main",
   components: {
     Card,
+  },
+  data() {
+    return {
+      options: {
+        params: {
+          page: 1,
+          ord: "asc",
+          limit: 10,
+          category: [1, 2, 3],
+        },
+      },
+    };
+  },
+  methods: {
+    setData() {
+      this.$axios
+        .get("https://problem.comento.kr/api/list", this.options)
+        .then((response) => {
+          console.log("뭘받아옴?", response.data);
+        });
+    },
   },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+Vue.prototype.$axios = axios; // prototype에 axios를 추가하여 Vue 인스턴스 내부안에서 별도로 import없이 this.$axios로 사용가능
 
 Vue.config.productionTip = false
+
 
 new Vue({
   render: h => h(App),
