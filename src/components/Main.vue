e4f93cba698e5a36a331316f750bc49ea81583ef
이재훈 <jami8291215@gmail.com>
2021-02-08 10:33:33 +0900

    무한 스크롤 적용으로 페이지 끝 도달시 새로운 페이지 정보 획득

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -44,11 +44,46 @@ export default {
           this.categoryArr = [...response.data.category];
         });
     },
+
+    // 무한 스크롤 피드 받아오기: API로부터 받아온 페이지 데이터를 이용해 다음 데이터 로드
+    fetchMoreFeeds() {
+      this.$axios
+        .get("https://problem.comento.kr/api/list", this.options)
+        .then((response) => {
+          console.log("next page", response.data.data);
+          // 기존 state 값의 불변성 유지를 위해 새로운 배열에 spread를 사용해 새로운 배열에 구현
+          this.feedArr = [...this.feedArr, ...response.data.data];
+          console.log("new Feeds", this.feedArr);
+        });
+    },
+
+    // 무한 스트롤 이벤트 핸들러
+    handleScroll() {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop = document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+
+      if (scrollTop + clientHeight >= scrollHeight) {
+        this.options.params.page = this.options.params.page + 1; // 기존 state의 page 값을 변경시킨 후 axios로 다음 페이지 feeds를 불러오게 한다.
+        // console.log("변경된 페이지", this.options.params.page);
+        // 페이지 끝에 도달하면 추가 데이터를 받아온다.
+        this.fetchMoreFeeds();
+      }
+    },
+  },
+  created: function () {
+    this.setData();
   },
   updated: function () {
-    console.log("feedArrt", this.feedArr);
-    console.log("categoryArr", this.categoryArr);
+    // console.log("feedArrt", this.feedArr);
+    // console.log("categoryArr", this.categoryArr);
+    // scroll event listner 등록
+    window.addEventListener("scroll", this.handleScroll);
   },
+  // scroll event listner 해제
+  // destroyed: function () {
+  //   window.removeEventListener("scroll", this.handleScroll);
+  // },
 };
 </script>
 
