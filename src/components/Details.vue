8f13e82ff5875113661e8d38c3c0a7d585bb24f2
이재훈 <jami8291215@gmail.com>
2021-02-09 17:28:03 +0900

    전반적인 그리드 완성

--- src/components/Details.vue
+++ src/components/Details.vue
@@ -1,18 +1,27 @@
 <template>
   <div>
-    <div class="container_details">
-      <div class="title_details">{{ details.title }}</div>
-      <div class="contents_details">{{ details.contents }}</div>
-      <div class="createdAt_details">{{ details.created_at }}</div>
-    </div>
-    <div class="container_reply">
-      <div class="count">답변 {{ details.reply.length }}</div>
-      <div v-for="item in details.reply" :key="item.id" id="detail">
-        <div class="box_reply">
-          <div>{{ item.user.name }}</div>
-          <div class="division_reply"></div>
-          <div>{{ item.contents }}</div>
-          <div>{{ item.created_at }}</div>
+    <Nav />
+    <div id="details">
+      <div class="container_details">
+        <div class="title_details">{{ details.title }}</div>
+        <div class="contents_details">{{ details.contents }}</div>
+        <div class="createdAt_details">
+          {{ details.created_at.substring(0, 10) }}
+        </div>
+      </div>
+      <div class="container_reply">
+        <div class="reply_title">
+          답변 <span class="count">{{ details.reply.length }}</span>
+        </div>
+        <div v-for="item in details.reply" :key="item.id" id="detail">
+          <div class="box_reply">
+            <div class="user_name">{{ item.user.name }}</div>
+            <div class="division_reply"></div>
+            <div class="contens_details">{{ item.contents }}</div>
+            <div class="created_at_details_reply">
+              {{ item.created_at.substring(0, 10) }}
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -20,8 +29,13 @@
 </template>
 
 <script>
+import Nav from "./Nav";
+
 export default {
   name: "Details",
+  components: {
+    Nav,
+  },
   data() {
     return {
       options: {
@@ -52,5 +66,86 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss" scoped>
+#details {
+  // border: 1px solid #e1e4e7;
+  // border-radius: 5px;
+  padding: 50px 45px;
+  // margin-bottom: 30px;
+
+  .container_details {
+    border: 1px solid #00c854;
+    border-radius: 5px;
+    padding: 0 30px;
+    padding-bottom: 22px;
+    margin-bottom: 30px;
+  }
+  .title_details {
+    font-weight: bold;
+    font-size: 18px;
+    margin-top: 25px;
+    margin-bottom: 15px;
+    color: #282c30;
+    line-height: 1.56;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .contents_details {
+    font-size: 16px;
+    color: #495057;
+    line-height: 1.56;
+
+    &:hover {
+      cursor: pointer;
+    }
+  }
+  .createdAt_details {
+    margin-top: 21px;
+    color: #adb5bd;
+    font-size: 13px;
+  }
+
+  .container_reply {
+    .reply_title {
+      line-height: 1.56;
+      color: #495057;
+
+      span {
+        color: #00c854;
+      }
+    }
+
+    .box_reply {
+      border: 1px solid #e1e4e7;
+      border-radius: 5px;
+      padding: 0 30px;
+      padding-bottom: 22px;
+      margin-bottom: 30px;
+
+      .user_name {
+        color: #7e848a;
+        margin-top: 28px;
+        line-height: 1.56;
+      }
+
+      .division_reply {
+        border: 1px solid #e1e4e7;
+        margin-top: 10px;
+        margin-bottom: 15px;
+      }
+
+      .contens_details {
+        line-height: 1.56;
+        color: #495057;
+        margin-bottom: 21px;
+      }
+      .created_at_details_reply {
+        color: #adb5bd;
+        font-size: 13px;
+      }
+    }
+  }
+}
 </style>
